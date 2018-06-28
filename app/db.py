90979dab0112d3bcfb8daf98874ab075c333f3bd
Michael Chau <michaelthechau@Gmail.com>
2018-06-28 15:33:09 -0700

    Included another database
    Included another database from freemysqlhosting.net for fun.

--- app/db.py
+++ app/db.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql9244283:bHnU6PW6AW@sql9.freemysqlhosting.net:3306/sql9244283'
 
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql3245126:Nc7QzXqKx1@sql3.freemysqlhosting.net:3306/sql3245126'
+#Just another one (Michael)
+
 Base = declarative_base()
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
